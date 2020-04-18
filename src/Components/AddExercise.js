33e336f5969a9806e7b06634c75c034d52187304
roosterdesign <neil@roosterdesign.co.uk>
2020-04-18 17:24:08 +0100

    add exercise

--- src/Components/AddExercise.js
+++ src/Components/AddExercise.js
@@ -1,14 +1,5 @@
-// TODO
-// Change "type" to select OR checkboxes (look into pushing to multiple collections) - update type from select value
-// Loading spinner
-// Validation and prop types
-// Make numbers not strings (fix issue when deleting all numbers from reps)
-// Redux
-// Style
-// Reset form button
-
-
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 import firebase from '../firebase'
 
 function AddExercise() {
@@ -56,23 +47,17 @@ function AddExercise() {
 
     const handleInputChange = (event, index) => {
         let { name, value } = event.target;
-        if(name === "type") {
-            console.log('update type')
-        } else if (name === "reps") {
+        if (name === "reps") {
             const updatedState = {...inputFields}
             updatedState.reps[index] = value;
             setInputFields(updatedState)
-        }
-         else {
+        } else {
             setInputFields({...inputFields, [name]: value })
         };
     };
 
     function onSubmit(e) {
         e.preventDefault();
-
-        alert(JSON.stringify(inputFields, null, 2));
-
         firebase
             .firestore()
             .collection('exercises')
@@ -80,27 +65,35 @@ function AddExercise() {
             .then(() => {
                 setExerciseType([])
                 setInputFields({
+                    type: '',
                     name: '',
                     sets: '',
                     reps: [''],
                     weight: ''
                 })
             })
+    };
+
+    function resetForm() {
+        setInputFields({
+            type: '',
+            name: '',
+            sets: '',
+            reps: [''],
+            weight: ''
+        })
     }
 
     return (
         <div>
             <h1>Add an exercise!</h1>
-            <pre>
-                {JSON.stringify(inputFields, null, 2)}
-            </pre>
             <form onSubmit={onSubmit}>
                 <div>
                     <label>Type</label>
                     <select name="type" type="text" value={inputFields.type} onChange={(event) => handleInputChange(event)} required>
                         <option value="">Please select...</option>
                         {exerciseType.map( type => (
-                            <option value={type.name} key={type.name}>{type.name}</option>
+                            <option value={type.id} key={type.name}>{type.name}</option>
                         ))}
                     </select>
                 </div>
@@ -120,7 +113,7 @@ function AddExercise() {
                             <div key={index}>
                                 <input name="reps" min="0" type="number" value={rep} onChange={event => handleInputChange(event, index)} required />
                                 <button type="button" onClick={() => handleAddRepsField()}>+ ADD</button>
-                                <button type="button" onClick={() => handleRemoveRepsField(index)}>- REMOVE</button>
+                                { inputFields.reps.length > 1 && <button type="button" onClick={() => handleRemoveRepsField(index)}>- REMOVE</button> }                                
                             </div>
                         ))
                     }
@@ -131,6 +124,7 @@ function AddExercise() {
                     <input name="weight" min="0" step="0.25" type="number" value ={inputFields.weight} onChange={event => handleInputChange(event)} required />
                 </div>
                 <button>Add Exercise</button>
+                <button type="button" onClick={() => resetForm()}>Reset</button>
             </form>
         </div>
     )
